85f4adf3066aabc192df80876089ca789c2dba69
saarhaber <saarhaber@gmail.com>
2019-07-17 20:26:32 -0400

    all functs

--- WebDev/Week 1/Day 3/javascript.js
+++ WebDev/Week 1/Day 3/javascript.js
@@ -115,48 +115,146 @@ function myEvery (arr,cb) {
 
 //reduce
 //Modifying Array prototype
-Array.prototype.myReduce = function (cb, accu) {
-?????
+Array.prototype.myReduce = function (cb, initial) {
+    if (initial == undefined) {
+      initial = 0;
+    }
+    let start = 0 + initial;
+    for(let i = 0; i < this.length; i++) {
+        start = cb(start, this[i]);
+    }
+    return start;
+}
+
 //reduce
 //Standalone
-????
-
+function myReduce (arr, initial) {
+    if (initial == undefined) {
+       initial = 0;
+     }
+     let sum = 0 + initial;
+   for (let i=0; i<arr.length; i++) {
+     sum+=arr[i];
+   }
+ return sum;
+ }
+ 
 //includes()
 //Modifying Array prototype
-
+Array.prototype.myIncludes = function (entry) {
+    for (let i=0; i<this.length; i++) {
+      let element = this[i];
+      if (element == entry) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 //includes()
 //Standalone
+function myIncludes(arr, entry) {
+    for (let i=0; i<arr.length; i++) {
+      let element = arr[i];
+      if (element == entry) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 //indexOf()
 //Modifying Array prototype
+Array.prototype.myIndexOf = function (entry, index) {
+  if (index == null) {
+    index = 0;
+  }
+  for (let i=0+index; i<this.length; i++) {
+    let element = this[i];
+    if (element == entry) {
+      return i;
+    }
+  }
+  return -1;
+}
 
 //indexOf()
 //Standalone
+function myIndexOf(arr, entry, index) {
+    if (index == null) {
+      index = 0;
+    }
+    for (let i=0+index; i<arr.length; i++) {
+      let element = arr[i];
+      if (element == entry) {
+        return i;
+      }
+    }
+    return -1;
+  }
 
 //push()
 //Modifying Array prototype
+Array.prototype.myPush = function (entry) {
+  this[this.length] = entry;
+  return this.length;
+}
 
 //push()
 //Standalone
+function myPush (arr, entry) {
+    arr[arr.length] = entry;
+    return arr.length;
+  }
 
 //lastIndexOf()
 //Modifying Array prototype
+Array.prototype.myLastIndexOf = function (entry) {
+  for (let i=this.length-1; i>=0; i--) {
+    let element = this[i];
+    if (entry == element) {
+      return i;
+    }
+  }
+  return -1;
+}
 
 //lastIndexOf()
 //Standalone
-
+function myLastIndexOf (arr, entry) {
+    for (let i=arr.length-1; i>=0; i--) {
+      let element = arr[i];
+      if (entry == element) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  
 //Object.keys()
 //Modifying Array prototype
 
 //Object.keys()
 //Standalone
+function grabKeys (obj) {
+    let arr2 = [];
+    for (let key in obj) {
+      arr2.push(key);
+    }
+  return arr2;
+  }
 
 //Object.values()
 //Modifying Array prototype
 
 //Object.values()
 //Standalone
-
+function grabKeys (obj) {
+    let arr2 = [];
+    for (let prop in obj) {
+      arr2.push(obj[prop]);
+    }
+  return arr2;
+  }
 
 
